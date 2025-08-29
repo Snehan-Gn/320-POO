85f1b3e8511760e1142c7d16d1a755b0dafab5a7
Snehan Gnanassorian <pr46eml@eduvaud.ch>
2025-08-29 11:22:05 +0200

    Ajout de snail procédural

---
+++ personnal/snail/ex_snail/ex_snail/Program.cs
@@ -0,0 +1,16 @@
+﻿int life = 50;
+Console.CursorVisible = false;
+Console.ForegroundColor = ConsoleColor.Green;
+
+for (int i = 0; i < life; i++)
+{
+    Console.Clear();
+    Console.SetCursorPosition(i, 10);
+    Console.Write("_@_ö");
+    Thread.Sleep(100);
+}
+
+Console.Clear();
+Console.SetCursorPosition(life, 10);
+Console.Write("____");
+Console.ReadLine();
